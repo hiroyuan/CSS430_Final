f830a988938a7bb2ad4be6739093dc6f6329389c
Hiro <Yoshihiro@D-10-156-27-100.dhcp4.washington.edu>
2018-02-28 14:37:54 -0800

    FileSystem.java added

---
+++ FileSystem.java
@@ -0,0 +1,79 @@
+public class FileSystem
+{
+	private SuperBlock superblock;
+	private Directory directory;
+	private FileTable filetable;
+
+	private final int SEEK_SET = 0;
+	private final int SEEK_CUR = 1;
+	private final int SEEK_END = 2;
+	
+	public FileSystem(int diskBlock)
+	{
+		superblock = new SuperBlock(diskBlocks);
+		
+		directory = new Directory(superblock.inodeBlocks);
+		
+		filetable = new FileTable(directory);
+		
+		FileTableEntry dirEnt = open("/" , "r");
+		int diSize = fsize(dirEnt);
+		if (dirSize > 0)
+		{
+			byte[] dirData = new byte[dirSize];
+			read(dirEnt, dirData);
+			directory.bytes2directory(dirData);
+		}
+		close(dirEnt);
+	}
+	
+	public void sync()
+	{
+		
+	}
+	public boolean format(int files) 
+	{
+		
+	}
+		
+	public FileTableEntry open(String filename, String mode)
+	{
+		
+	}	
+		
+	public boolean close(FileTableEntry ftEnt) 
+	{
+		
+	}
+		
+	public int fsize(FileTableEntry ftEnt)
+	{
+		
+	}
+		
+		
+	public int read(FileTableEntry ftEnt, byte[] buffer)
+	{
+		
+	}
+		
+	public int write(FileTableEntry ftEnt, byte[] buffer)
+	{
+		
+	}	
+		
+	public boolean delete(String filename)
+	{
+		
+	}
+		
+	public int seek(FileTableEntry ftEnt, int offset, int whence)
+	{
+	
+	}
+
+	private boolean deallocAllBlocks(FileTableEntry ftEnt)
+	{
+		
+	}
+}
