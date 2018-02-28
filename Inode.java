d95b2f99bfae20578a4deece097b095e90bfbb38
tunglequang <tunglq89@uw.edu>
2018-02-28 14:18:21 -0800

    Adding SuperBlock, Inode and Directory

---
+++ Inode.java
@@ -0,0 +1,28 @@
+public class Inode{
+	private final static int iNodeSize = 32;			//Fix size for an Inode is 32 bytes
+	private final static int directSize = 11;			// # of direct Pointers
+	
+	public int length;			//File sizes in bytes
+	public short count;			//# file-table entries point to this
+	public short flag; 			//0 = unused, 1= used
+	public short direct[] = new short[directSize];	//Direct pointers
+	public short indirect;			//Indirect pointer
+	
+	//Default constructor
+	Inode(){
+		length = 0;
+		count = 0;
+		flag = 1;
+		
+		for(int i=0;i<directSize;i++)
+			direct[i] = -1;					//Point to nothing
+		indirect = -1;
+	}
+	
+	Inode(short iNumber){		//Retrieve the Inode from disk
+		//Need to implement
+	}
+	int toDisk(short iNumber){	//Save to the disk as i-th inode
+		//Need to implement		
+	}
+}
---
+++ SuperBlock.java
@@ -0,0 +1,5 @@
+class SuperBlock{
+	public int totalBlock;			//The number of disk block
+	public int totalInodes;			//The number of Inodes
+	public int freeList;			//The block number of the free list's head
+}
